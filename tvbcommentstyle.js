0139ee20db4f3457847c356ac9aa62f29c5420c7
Kr4 <58572705+Kr4@users.noreply.github.com>
2019-12-05 22:38:55 -0500

    Update tvbcommentstyle.js

--- tvbcommentstyle.js
+++ tvbcommentstyle.js
@@ -8,19 +8,19 @@ var jsCommentPages = function(){
 					.addClass("js-inactive-tab");
 				switch ($tab.attr("id")){
 					case "blogger-comments": 
-						$tab.prepend("<img src='http://lh6.googleusercontent.com/-_InM8Yxvqlg/Tg03QmGCkDI/AAAAAAAAAd8/Ozlnv5wP_C8/s800/blogger-white-B.png'>");
+						$tab.prepend("<img src='https://lh6.googleusercontent.com/-_InM8Yxvqlg/Tg03QmGCkDI/AAAAAAAAAd8/Ozlnv5wP_C8/s800/blogger-white-B.png'>");
 						break;
 					case "disqus-comments":
-						$tab.prepend("<img src='http://4.bp.blogspot.com/-Vh58qAXwdcw/UcbkHl31HMI/AAAAAAAAEJ0/QndAwVtUZAI/s1600/white-disqus-D.png'>");
+						$tab.prepend("<img src='https://4.bp.blogspot.com/-Vh58qAXwdcw/UcbkHl31HMI/AAAAAAAAEJ0/QndAwVtUZAI/s1600/white-disqus-D.png'>");
 						break;
 					case "fb-comments":
-						$tab.prepend("<img src='http://3.bp.blogspot.com/-WZFew-hmWYs/UcbkHj-2D2I/AAAAAAAAEJw/D0mXysXQP_Q/s1600/white-Facebook-F.png'>");
+						$tab.prepend("<img src='https://3.bp.blogspot.com/-WZFew-hmWYs/UcbkHj-2D2I/AAAAAAAAEJw/D0mXysXQP_Q/s1600/white-Facebook-F.png'>");
 						break;
 					case "get-this":
-						$tab.prepend("<img src='http://3.bp.blogspot.com/-9dSC4V-ERMs/UclFdUjqV_I/AAAAAAAAEKQ/R0F5B-uHzwM/s1600/Get-this.png'>");
+						$tab.prepend("<img src='https://3.bp.blogspot.com/-9dSC4V-ERMs/UclFdUjqV_I/AAAAAAAAEKQ/R0F5B-uHzwM/s1600/Get-this.png'>");
 						break;
 					case "gplus-comments":
-						$tab.prepend("<img src='http://2.bp.blogspot.com/-oAGoAXHbA1s/UdVqbMTj5yI/AAAAAAAAENU/b0lZ9QaO0fc/s210/white-Google-Plus.png'>");
+						$tab.prepend("<img src='https://2.bp.blogspot.com/-oAGoAXHbA1s/UdVqbMTj5yI/AAAAAAAAENU/b0lZ9QaO0fc/s210/white-Google-Plus.png'>");
 						break;
 						
 				}
@@ -45,7 +45,7 @@ var jsCommentPages = function(){
 		  	$(".js-page-tweet-count").each(
 				function(){
 					var $count = $(this);
-					$.getJSON("http://urls.api.twitter.com/1/urls/count.json?callback=?",
+					$.getJSON("https://urls.api.twitter.com/1/urls/count.json?callback=?",
 		      	{url: $count.attr("href")},
 		         function(json){$count.text(json.count);$count = null;});					   	
 				}
@@ -62,6 +62,5 @@ var jsCommentPages = function(){
 		  	$activePage = $("#" + $activeTab.attr("id") + "-page");
 		  	$activePage.show();
 		};
-	$("head").append("<link id='js-comments-pages-styles' rel='stylesheet' type='text/css' href='https://raw.githubusercontent.com/Kr4/Comment_Style.github.io/master/tvbcommentstyle.css'/>");
   	$("document").ready(init);
 }();
